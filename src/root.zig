8c07ac2509c103705cf57598dee8c03fc2e435f0
Mu Lu <murphytalk@gmail.com>
2024-08-26 00:19:33 +0900

    use structt ImgFile to collect full path of all image files

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,8 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 const testing = std.testing;
 
 pub fn doWork(img_dir: []const u8, bin_dir: []const u8) !void {
-    _ = bin_dir;
-    try walkImgDir(img_dir);
-}
-
-pub fn walkImgDir(img_dir: []const u8) !void {
-    var dir = try std.fs.openDirAbsolute(img_dir, .{ .iterate = true });
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
@@ -18,11 +12,32 @@ pub fn walkImgDir(img_dir: []const u8) !void {
 
     const arena_allocator = arena.allocator();
 
-    var walker = try dir.walk(arena_allocator);
+    _ = bin_dir;
+    var imageFiles = try walkImgDir(arena_allocator, img_dir);
+    defer imageFiles.deinit();
+
+    for (imageFiles.items) |f| {
+        std.debug.print("File path: {s}\n", .{f.fullPath});
+    }
+}
+
+pub const ImgFile = struct {
+    fullPath: []const u8,
+    pub fn init(path: []const u8) ImgFile {
+        return ImgFile{ .fullPath = path };
+    }
+};
+
+pub fn walkImgDir(allocator: std.mem.Allocator, img_dir: []const u8) !ArrayList(ImgFile) {
+    var files = ArrayList(ImgFile).init(allocator);
+    var dir = try std.fs.openDirAbsolute(img_dir, .{ .iterate = true });
+
+    var walker = try dir.walk(allocator);
     defer walker.deinit();
     while (try walker.next()) |entry| {
         const dirs = [_][]const u8{ img_dir, entry.path, entry.basename };
-        const path_buffer = try std.fs.path.join(arena_allocator, &dirs);
-        std.debug.print("File path: {s}\n", .{path_buffer});
+        const imgFile = ImgFile.init(try std.fs.path.join(allocator, &dirs));
+        try files.append(imgFile);
     }
+    return files;
 }
